1c7211112714eddcc2ae71c705bde3f38dad3921
jlp <mattlukin@gmail.com>
2023-01-25 15:12:24 +0000

    Added Cesium.js example

---
+++ examples/web-ifc-three/cesium/index.js
@@ -0,0 +1,98 @@
+// Grant CesiumJS access to your ion assets
+//this access token is restricted to this github repo. Make a free account to get your own: https://ion.cesium.com/
+Cesium.Ion.defaultAccessToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhNjkzMzFhZC03ZjgwLTQ2NmItODU0ZS0xZGRmN2ViNzQ0ZTUiLCJpZCI6NTg1NjAsImlhdCI6MTY3NDU2MzU5Nn0.1fIBYuxgsizyyPiOMnE9uUSyqGSXTuC9rd93TamHJdk";
+
+// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+const viewer = new Cesium.Viewer("cesiumContainer", {
+  terrainProvider: Cesium.createWorldTerrain(),
+});
+
+// Add Cesium OSM Buildings, a global 3D buildings layer.
+const buildingTileset = viewer.scene.primitives.add(
+  Cesium.createOsmBuildings()
+);
+
+let htmlElem;
+
+// define building position and orientation
+const position = Cesium.Cartesian3.fromDegrees(-2.261282, 53.398754, 88);
+const heading = Cesium.Math.toRadians(25.0);
+const pitch = Cesium.Math.toRadians(0.0);
+const roll = Cesium.Math.toRadians(0.0);
+const orientation = Cesium.Transforms.headingPitchRollQuaternion(
+  position,
+  new Cesium.HeadingPitchRoll(heading, pitch, roll)
+);
+
+(async () => {
+  "use strict";
+  try {
+    const resource = await Cesium.IonResource.fromAssetId(1511359);
+    const entity = viewer.entities.add({
+      position: position,
+      model: {
+        uri: resource,
+      },
+      description: htmlElem,
+      orientation: orientation,
+    });
+    viewer.zoomTo(entity); //zoom to the building on start up
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+getJSONProperties();
+
+async function getJSONProperties() {
+  try {
+    const rawProperties = await fetch("./model_properties/properties.json"); //using model 05.ifc from the IFC.js IFC files
+    const properties = await rawProperties.json();
+
+    for (let elem in properties) {
+      if (properties[elem].type === "IFCBUILDING") {
+        htmlElem = createDescription(
+          Object.keys(properties[elem]).length,
+          Object.keys(properties[elem]),
+          Object.values(properties[elem])
+        );
+      }
+    }
+
+    return htmlElem;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//create the html element to be inserted into 'description' and shown in Cesium's infoBox (when you click on the model)
+
+function createDescription(numberOfEntries, keys, values) {
+  let tableElem = document.createElement("table");
+  tableElem.setAttribute("class", "cesium-infoBox-defaultTable");
+
+  let tableBodyElem = document.createElement("tbody");
+
+  //create table entries based on ifc data
+  for (let i = 0; i < numberOfEntries; i++) {
+    let tableEntryElem = document.createElement("tr");
+
+    let thElem = document.createElement("th");
+    thElem.innerText = keys[i];
+
+    let tdElem = document.createElement("td");
+    tdElem.innerText = values[i];
+
+    tableEntryElem.appendChild(thElem);
+    tableEntryElem.appendChild(tdElem);
+
+    tableBodyElem.appendChild(tableEntryElem);
+  }
+
+  tableElem.appendChild(tableBodyElem);
+
+  let description = tableElem.outerHTML; //need to provide this in text format for Cesium infoBox
+
+  return description;
+}
